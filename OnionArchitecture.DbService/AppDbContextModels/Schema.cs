ff2d5c1539676cec3e66767ea3dbccd91aa3fc76
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-17 14:16:53 +0630

    Revert "-"
    This reverts commit 8fb6944c3b9b66bc02921aca8385d60792bfbec6.

--- OnionArchitecture.DbService/AppDbContextModels/Schema.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace OnionArchitecture.DbService.AppDbContextModels;
-
-public partial class Schema
-{
-    public int Version { get; set; }
-}
