a5de154110d18c5521afe54e66ba794605d660ab
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-17 15:20:03 +0630

    -

--- OnionArchitecture.DTOs/Features/Blog/BlogRequestModel.cs
+++ OnionArchitecture.DTOs/Features/Blog/BlogRequestModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OnionArchitecture.DTOs.Features.Blog
+﻿namespace OnionArchitecture.DTOs.Features.Blog
 {
 	internal class BlogRequestModel
 	{
