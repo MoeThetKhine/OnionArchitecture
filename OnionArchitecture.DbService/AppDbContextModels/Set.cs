033580617f4e093e17e9f4d736388db67126e22c
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-17 14:32:14 +0630

    -

--- OnionArchitecture.DbService/AppDbContextModels/Set.cs
+++ OnionArchitecture.DbService/AppDbContextModels/Set.cs
@@ -1,5 +1,7 @@
 ﻿namespace OnionArchitecture.DbService.AppDbContextModels;
 
+#region Set
+
 public partial class Set
 {
     public string Key { get; set; } = null!;
@@ -10,3 +12,5 @@ public partial class Set
 
     public DateTime? ExpireAt { get; set; }
 }
+
+#endregion
