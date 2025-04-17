ebcb7af92d0552e57b6c061fe78cc8a4c824dac6
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-17 15:41:29 +0630

    Feat : Add BlogListModelV1

--- OnionArchitecture.DTOs/Features/Blog/BlogListModel.cs
+++ OnionArchitecture.DTOs/Features/Blog/BlogListModel.cs
@@ -7,3 +7,9 @@ public class BlogListModel
 	public IEnumerable<BlogModel> DataLst { get; set; }
 	public PageSettingModel PageSetting { get; set; }
 }
+
+public class BlogListModelV1
+{
+	public IQueryable<BlogModel> DataLst { get; set; }
+	public PageSettingModel PageSetting { get; set; }
+}
