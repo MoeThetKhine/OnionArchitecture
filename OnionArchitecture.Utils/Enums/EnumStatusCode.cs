59bca977f5ffe331c3871f7f4695d30dcb7b5a4c
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-17 16:05:54 +0630

    Feat : Add EnumStatusCode

--- OnionArchitecture.Utils/Enums/EnumStatusCode.cs
+++ OnionArchitecture.Utils/Enums/EnumStatusCode.cs
@@ -2,4 +2,13 @@
 
 public enum EnumStatusCode
 {
+	None,
+	Success = 200,
+	Created = 201,
+	Accepted = 202,
+	BadRequest = 400,
+	NotFound = 404,
+	Conflict = 409,
+	Locked = 423,
+	InternalServerError = 500
 }
