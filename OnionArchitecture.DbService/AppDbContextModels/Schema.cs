8fb6944c3b9b66bc02921aca8385d60792bfbec6
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-17 14:15:50 +0630

    -

---
+++ OnionArchitecture.DbService/AppDbContextModels/Schema.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnionArchitecture.DbService.AppDbContextModels;
+
+public partial class Schema
+{
+    public int Version { get; set; }
+}
