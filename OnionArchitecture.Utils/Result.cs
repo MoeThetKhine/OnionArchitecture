102bd56279a16d63f9dbf50598049e451d8dd7bd
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-17 16:21:59 +0630

    Feat : Add Result

--- OnionArchitecture.Utils/Result.cs
+++ OnionArchitecture.Utils/Result.cs
@@ -1,5 +1,12 @@
-﻿namespace OnionArchitecture.Utils;
+﻿using OnionArchitecture.Utils.Enums;
+
+namespace OnionArchitecture.Utils;
 
 public class Result<T>
 {
+	public T Data { get; set; }
+	public string Message { get; set; }
+	public bool IsSuccess { get; set; }
+	public EnumStatusCode StatusCode { get; set; }
+
 }
