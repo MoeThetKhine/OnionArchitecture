b419e456ab36d473178ea1cc1b4398c12a670f68
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-17 14:22:12 +0630

    -

---
+++ OnionArchitecture.DbService/AppDbContextModels/TaskCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnionArchitecture.DbService.AppDbContextModels;
+
+public partial class TaskCategory
+{
+    public int CategoryId { get; set; }
+
+    public string CategoryName { get; set; } = null!;
+
+    public bool? IsDelete { get; set; }
+
+    public virtual ICollection<ToDoList> ToDoLists { get; set; } = new List<ToDoList>();
+}
