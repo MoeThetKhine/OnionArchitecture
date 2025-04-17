e17a3863b53cc554966c5cfbd595574591dfa3c7
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-17 16:08:22 +0630

    Feat : Add BlogModel ToModel

--- OnionArchitecture.Extension/Extension.cs
+++ OnionArchitecture.Extension/Extension.cs
@@ -1,5 +1,21 @@
-﻿namespace OnionArchitecture.Extension;
+﻿using OnionArchitecture.DbService.AppDbContextModels;
+using OnionArchitecture.DTOs.Features.Blog;
 
-public class Extension
+namespace OnionArchitecture.Extension;
+
+public static class Extension
 {
+
+	public static BlogModel ToModel(this TblBlog dataModel)
+	{
+		return new BlogModel
+		{
+			BlogId = dataModel.BlogId,
+			BlogTitle = dataModel.BlogTitle,
+			BlogAuthor = dataModel.BlogAuthor,
+			BlogContent = dataModel.BlogContent,
+			DeleteFlag = dataModel.DeleteFlag
+		};
+	}
+
 }
