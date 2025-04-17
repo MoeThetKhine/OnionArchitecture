44aafffa95ff6424e50f4cb863ead60158859aea
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-17 15:51:13 +0630

    -

--- OnionArchitecture.DTOs/Features/Blog/BlogRequestModel.cs
+++ OnionArchitecture.DTOs/Features/Blog/BlogRequestModel.cs
@@ -1,8 +1,12 @@
 ﻿namespace OnionArchitecture.DTOs.Features.Blog;
 
+#region BlogRequestModel
+
 public class BlogRequestModel
 {
 	public string BlogTitle { get; set; } = null!;
 	public string BlogAuthor { get; set; } = null!;
 	public string BlogContent { get; set; } = null!;
 }
+
+#endregion
