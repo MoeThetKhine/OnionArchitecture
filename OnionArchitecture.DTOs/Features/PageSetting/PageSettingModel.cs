94bef6e7b6d7e2e2d750ebca19c2ac62b0b49dd7
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-17 15:25:30 +0630

    -

--- OnionArchitecture.DTOs/Features/PageSetting/PageSettingModel.cs
+++ OnionArchitecture.DTOs/Features/PageSetting/PageSettingModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OnionArchitecture.DTOs.Features.PageSetting
+﻿namespace OnionArchitecture.DTOs.Features.PageSetting
 {
 	internal class PageSettingModel
 	{
