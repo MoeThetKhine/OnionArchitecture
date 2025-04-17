62d038130e4222a817066462a0f7b65891a9e87b
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-17 16:00:29 +0630

    -

--- OnionArchitecture.Utils/Resources/MessageResource.cs
+++ OnionArchitecture.Utils/Resources/MessageResource.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OnionArchitecture.Utils.Resources
+﻿namespace OnionArchitecture.Utils.Resources
 {
 	internal class MessageResource
 	{
