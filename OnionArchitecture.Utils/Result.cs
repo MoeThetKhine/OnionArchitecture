965a6eb761c76b0e7a69267421fd156a5b85964e
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-17 16:22:43 +0630

    Feat : Add Success

--- OnionArchitecture.Utils/Result.cs
+++ OnionArchitecture.Utils/Result.cs
@@ -9,4 +9,14 @@ public class Result<T>
 	public bool IsSuccess { get; set; }
 	public EnumStatusCode StatusCode { get; set; }
 
+
+	public static Result<T> Success(string message = "Success", EnumStatusCode statusCode = EnumStatusCode.Success)
+	{
+		return new Result<T>
+		{
+			IsSuccess = true,
+			Message = message,
+			StatusCode = statusCode
+		};
+	}
 }
