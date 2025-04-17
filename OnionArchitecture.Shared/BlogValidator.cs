4acafd472acba1b2b83a1ee6620b435f4b2054af
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-17 16:14:04 +0630

    Feat : Add BlogValidator

--- OnionArchitecture.Shared/BlogValidator.cs
+++ OnionArchitecture.Shared/BlogValidator.cs
@@ -1,5 +1,24 @@
-﻿namespace OnionArchitecture.Shared;
+﻿using FluentValidation;
+using OnionArchitecture.DTOs.Features.Blog;
 
-public class BlogValidator
+namespace OnionArchitecture.Shared;
+
+public class BlogValidator : AbstractValidator<BlogRequestModel>
 {
+
+	public BlogValidator()
+	{
+		RuleFor(x => x.BlogTitle)
+			.NotEmpty().WithMessage("Blog Title cannot be empty.")
+			.NotNull().WithMessage("Blog Title cannot be null.");
+
+		RuleFor(x => x.BlogAuthor)
+			 .NotEmpty().WithMessage("Blog Author cannot be empty.")
+			.NotNull().WithMessage("Blog Author cannot be null.");
+
+		RuleFor(x => x.BlogContent)
+		   .NotEmpty().WithMessage("Blog Content cannot be empty.")
+		   .NotNull().WithMessage("Blog Content cannot be null.");
+	}
+
 }
