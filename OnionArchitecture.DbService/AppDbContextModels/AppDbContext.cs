a7b1d33954a4cc0590af3814c3a8e8deb3058839
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-17 15:03:31 +0630

    -

--- OnionArchitecture.DbService/AppDbContextModels/AppDbContext.cs
+++ OnionArchitecture.DbService/AppDbContextModels/AppDbContext.cs
@@ -272,6 +272,8 @@ public partial class AppDbContext : DbContext
 
 		#endregion
 
+		#region ToDoList
+
 		modelBuilder.Entity<ToDoList>(entity =>
         {
             entity.HasKey(e => e.TaskId).HasName("PK__ToDoList__7C6949D1F4B3258B");
@@ -297,7 +299,9 @@ public partial class AppDbContext : DbContext
                 .HasConstraintName("FK__ToDoList__Catego__4E88ABD4");
         });
 
-        OnModelCreatingPartial(modelBuilder);
+		#endregion
+
+		OnModelCreatingPartial(modelBuilder);
     }
 
 	#endregion
