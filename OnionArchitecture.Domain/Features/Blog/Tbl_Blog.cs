c97453eccf4c4a417d3628ca2c5d9c0734853e9c
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-18 23:24:06 +0630

    Feat : Add Tbl_Blog

--- OnionArchitecture.Domain/Features/Blog/Tbl_Blog.cs
+++ OnionArchitecture.Domain/Features/Blog/Tbl_Blog.cs
@@ -2,4 +2,14 @@
 
 public class Tbl_Blog
 {
+	public long BlogId { get; set; }
+
+	public string BlogTitle { get; set; } = null!;
+
+	public string BlogAuthor { get; set; } = null!;
+
+	public string BlogContent { get; set; } = null!;
+
+	public bool? DeleteFlag { get; set; }
+
 }
