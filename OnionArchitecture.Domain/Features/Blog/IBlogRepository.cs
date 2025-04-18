f03b21a7985b80f8e13df49811e27f44bda80afe
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-18 17:42:53 +0630

    -

---
+++ OnionArchitecture.Domain/Features/Blog/IBlogRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArchitecture.Domain.Features.Blog
+{
+	internal interface IBlogRepository
+	{
+	}
+}
