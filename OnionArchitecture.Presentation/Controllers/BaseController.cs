7c75553b7edf7f4e3067b0917deefef9377ded1b
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-18 17:44:09 +0630

    -

---
+++ OnionArchitecture.Presentation/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnionArchitecture.Presentation.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class BaseController : ControllerBase
+	{
+	}
+}
