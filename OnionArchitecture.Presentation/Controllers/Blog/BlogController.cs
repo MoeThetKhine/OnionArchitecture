ddae42ae4db702d3cf6e4d24cb4636c9b1c8e624
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-18 22:21:21 +0630

    Format Code

--- OnionArchitecture.Presentation/Controllers/Blog/BlogController.cs
+++ OnionArchitecture.Presentation/Controllers/Blog/BlogController.cs
@@ -1,10 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace OnionArchitecture.Presentation.Controllers.Blog
+namespace OnionArchitecture.Presentation.Controllers.Blog;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : ControllerBase
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class BlogController : ControllerBase
-	{
-	}
 }
