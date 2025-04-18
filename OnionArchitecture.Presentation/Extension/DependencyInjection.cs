001cc522ce85cde403e80b0055a284fd99c79f9c
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-18 23:29:35 +0630

    Feat : Add AddDbContextService

--- OnionArchitecture.Presentation/Extension/DependencyInjection.cs
+++ OnionArchitecture.Presentation/Extension/DependencyInjection.cs
@@ -1,7 +1,24 @@
-﻿namespace OnionArchitecture.Presentation.Extension;
+﻿using Microsoft.EntityFrameworkCore;
+using OnionArchitecture.Infrastructure.Features.Blog;
+
+namespace OnionArchitecture.Presentation.Extension;
 
 public static class DependencyInjection
 {
 
+	private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+	{
+		builder.Services.AddDbContext<BlogDbContext>(
+			opt =>
+			{
+				opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+				opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+			},
+			ServiceLifetime.Transient,
+			ServiceLifetime.Transient
+		);
+
+		return services;
+	}
 
 }
