48e8007d0151156d4bfd2a638b7490fb0427bfe4
Moe Thet Khine <soniapoudel03@gmail.com>
2025-04-18 17:44:02 +0630

    -

---
+++ OnionArchitecture.Presentation/Controllers/Blog/BlogController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnionArchitecture.Presentation.Controllers.Blog
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class BlogController : ControllerBase
+	{
+	}
+}
