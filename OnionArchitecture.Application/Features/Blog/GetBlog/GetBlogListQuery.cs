33bc7dfc9c8d97c9c6e90ec5f003b6e86c6419b1
Moe Thet Khine <soniapoudel03@gmail.com>
2026-04-01 11:39:18 +0630

    -

--- OnionArchitecture.Application/Features/Blog/GetBlog/GetBlogListQuery.cs
+++ OnionArchitecture.Application/Features/Blog/GetBlog/GetBlogListQuery.cs
@@ -1,5 +1,16 @@
-﻿namespace OnionArchitecture.Application.Features.Blog.GetBlog;
+﻿using OnionArchitecture.DTOs.Features.Blog;
+using OnionArchitecture.Utils;
 
-public class GetBlogListQuery
+namespace OnionArchitecture.Application.Features.Blog.GetBlog;
+
+public class GetBlogListQuery : IRequest<Result<BlogListModelV1>>
 {
+	public int PageNo { get; set; }
+	public int PageSize { get; set; }
+
+	public GetBlogListQuery(int pageNo, int pageSize)
+	{
+		PageNo = pageNo;
+		PageSize = pageSize;
+	}
 }
