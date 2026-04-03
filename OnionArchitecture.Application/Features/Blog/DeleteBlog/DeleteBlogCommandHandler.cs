0ef7c89a9b610f323145a705489857dc80f477c2
Moe Thet Khine <soniapoudel03@gmail.com>
2026-04-03 21:25:08 +0630

    -

--- OnionArchitecture.Application/Features/Blog/DeleteBlog/DeleteBlogCommandHandler.cs
+++ OnionArchitecture.Application/Features/Blog/DeleteBlog/DeleteBlogCommandHandler.cs
@@ -9,6 +9,8 @@ public class DeleteBlogCommandHandler : IRequestHandler<DeleteBlogCommand, Resul
 		_blogRepository = blogRepository;
 	}
 
+	#region Handle
+
 	public async Task<Result<BlogModel>> Handle(DeleteBlogCommand request, CancellationToken cancellationToken)
 	{
 		Result<BlogModel> result;
@@ -24,5 +26,7 @@ public class DeleteBlogCommandHandler : IRequestHandler<DeleteBlogCommand, Resul
 	result:
 		return result;
 	}
+
+	#endregion
 
 }
