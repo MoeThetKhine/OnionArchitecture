72aa4038c2bdb830fb6ab3b9db2e94083073a3b1
Moe Thet Khine <soniapoudel03@gmail.com>
2026-04-03 16:58:34 +0630

    Feat : Add DeleteBlogCommandHandler

--- OnionArchitecture.Application/Features/Blog/DeleteBlog/DeleteBlogCommandHandler.cs
+++ OnionArchitecture.Application/Features/Blog/DeleteBlog/DeleteBlogCommandHandler.cs
@@ -1,5 +1,34 @@
-﻿namespace OnionArchitecture.Application.Features.Blog.DeleteBlog;
+﻿using MediatR;
+using OnionArchitecture.Domain.Features.Blog;
+using OnionArchitecture.DTOs.Features.Blog;
+using OnionArchitecture.Utils.Resources;
+using OnionArchitecture.Utils;
 
-public class DeleteBlogCommandHandler
+namespace OnionArchitecture.Application.Features.Blog.DeleteBlog;
+
+public class DeleteBlogCommandHandler : IRequestHandler<DeleteBlogCommand, Result<BlogModel>>
 {
+	private readonly IBlogRepository _blogRepository;
+
+	public DeleteBlogCommandHandler(IBlogRepository blogRepository)
+	{
+		_blogRepository = blogRepository;
+	}
+
+	public async Task<Result<BlogModel>> Handle(DeleteBlogCommand request, CancellationToken cancellationToken)
+	{
+		Result<BlogModel> result;
+
+		if (request.BlogId <= 0)
+		{
+			result = Result<BlogModel>.Fail(MessageResource.InvalidId);
+			goto result;
+		}
+
+		result = await _blogRepository.DeleteBlogAsync(request.BlogId, cancellationToken);
+
+	result:
+		return result;
+	}
+
 }
