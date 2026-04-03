33eaad0cc04985466c76d77d474da2ff5aa605ea
Moe Thet Khine <soniapoudel03@gmail.com>
2026-04-03 17:22:34 +0630

    Feat : Add CreateBlogCommand

--- OnionArchitecture.Application/Features/Blog/CreateBlog/CreateBlogCommand.cs
+++ OnionArchitecture.Application/Features/Blog/CreateBlog/CreateBlogCommand.cs
@@ -1,5 +1,16 @@
-﻿namespace OnionArchitecture.Application.Features.Blog.CreateBlog;
+﻿using MediatR;
+using OnionArchitecture.DTOs.Features.Blog;
+using OnionArchitecture.Utils;
 
-public class CreateBlogCommand
+namespace OnionArchitecture.Application.Features.Blog.CreateBlog;
+
+public class CreateBlogCommand : IRequest<Result<BlogModel>>
 {
+	public BlogRequestModel requestModel;
+
+	public CreateBlogCommand(BlogRequestModel requestModel)
+	{
+		this.requestModel = requestModel;
+	}
+
 }
