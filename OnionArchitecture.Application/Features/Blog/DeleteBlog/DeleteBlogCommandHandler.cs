0581b90fac277ea5783251b8cc379afaaa107b04
Moe Thet Khine <soniapoudel03@gmail.com>
2026-04-03 21:25:31 +0630

    -

--- OnionArchitecture.Application/Features/Blog/DeleteBlog/DeleteBlogCommandHandler.cs
+++ OnionArchitecture.Application/Features/Blog/DeleteBlog/DeleteBlogCommandHandler.cs
@@ -1,5 +1,7 @@
 ﻿namespace OnionArchitecture.Application.Features.Blog.DeleteBlog;
 
+#region DeleteBlogCommandHandler
+
 public class DeleteBlogCommandHandler : IRequestHandler<DeleteBlogCommand, Result<BlogModel>>
 {
 	private readonly IBlogRepository _blogRepository;
@@ -30,3 +32,5 @@ public class DeleteBlogCommandHandler : IRequestHandler<DeleteBlogCommand, Resul
 	#endregion
 
 }
+
+#endregion
