14702ffb4807889a18f6d98c265d268f4d56a408
Moe Thet Khine <soniapoudel03@gmail.com>
2026-04-03 17:23:21 +0630

    Feat : Add UpdateBlogCommandHandler

--- OnionArchitecture.Application/Features/Blog/UpdateBlog/UpdateBlogCommandHandler.cs
+++ OnionArchitecture.Application/Features/Blog/UpdateBlog/UpdateBlogCommandHandler.cs
@@ -1,5 +1,46 @@
-﻿namespace OnionArchitecture.Application.Features.Blog.UpdateBlog;
+﻿using MediatR;
+using Microsoft.IdentityModel.Tokens;
+using OnionArchitecture.Domain.Features.Blog;
+using OnionArchitecture.DTOs.Features.Blog;
+using OnionArchitecture.Utils;
 
-public class UpdateBlogCommandHandler
+namespace OnionArchitecture.Application.Features.Blog.UpdateBlog;
+
+public class UpdateBlogCommandHandler : IRequestHandler<UpdateBlogCommand, Result<BlogModel>>
 {
+	private readonly IBlogRepository _blogRepository;
+
+	public UpdateBlogCommandHandler(IBlogRepository blogRepository)
+	{
+		_blogRepository = blogRepository;
+	}
+
+	public async Task<Result<BlogModel>> Handle(UpdateBlogCommand request, CancellationToken cancellationToken)
+	{
+		Result<BlogModel> result;
+
+		if (request.RequestModel.BlogTitle.IsNullOrEmpty())
+		{
+			result = Result<BlogModel>.Fail("Blog Title cannot be empty.");
+			goto result;
+		}
+
+		if (request.RequestModel.BlogAuthor.IsNullOrEmpty())
+		{
+			result = Result<BlogModel>.Fail("Blog Author cannot be empty.");
+			goto result;
+		}
+
+		if (request.RequestModel.BlogContent.IsNullOrEmpty())
+		{
+			result = Result<BlogModel>.Fail("Blog Content cannot be empty.");
+			goto result;
+		}
+
+		result = await _blogRepository.UpdateBlogAsync(request.RequestModel, request.BlogId, cancellationToken);
+
+	result:
+		return result;
+	}
 }
+
