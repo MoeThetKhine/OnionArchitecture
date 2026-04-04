40cec72aea536efd04ecd1d7bd5d477e0e52b34f
Moe Thet Khine <soniapoudel03@gmail.com>
2026-04-04 10:02:44 +0630

    -

--- OnionArchitecture.Infrastructure/Features/Blog/BlogRepository.cs
+++ OnionArchitecture.Infrastructure/Features/Blog/BlogRepository.cs
@@ -1,5 +1,13 @@
-﻿namespace OnionArchitecture.Infrastructure.Features.Blog;
+﻿using OnionArchitecture.DbService.AppDbContextModels;
+
+namespace OnionArchitecture.Infrastructure.Features.Blog;
 
 public class BlogRepository : IBlogRepository
 {
+	private readonly AppDbContext _appDbContext;
+
+	public BlogRepository(AppDbContext appDbContext)
+	{
+		_appDbContext = appDbContext;
+	}
 }
