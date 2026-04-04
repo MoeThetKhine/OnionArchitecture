abe95857422e7add1937ff69beaffa698348d3a0
Moe Thet Khine <soniapoudel03@gmail.com>
2026-04-04 09:35:45 +0630

    Feat : Add GetBlogsAsync in IBlogRepository

--- OnionArchitecture.Domain/Features/Blog/IBlogRepository.cs
+++ OnionArchitecture.Domain/Features/Blog/IBlogRepository.cs
@@ -1,5 +1,9 @@
-﻿namespace OnionArchitecture.Domain.Features.Blog;
+﻿using OnionArchitecture.DTOs.Features.Blog;
+using OnionArchitecture.Utils;
+
+namespace OnionArchitecture.Domain.Features.Blog;
 
 public interface IBlogRepository
 {
+	Task<Result<BlogListModelV1>> GetBlogsAsync(int pageNo, int pageSize, CancellationToken cancellationToken);
 }
