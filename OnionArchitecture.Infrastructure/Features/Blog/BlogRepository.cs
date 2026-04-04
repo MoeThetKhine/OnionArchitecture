1b01c2896a07085ef08b2784e7b69cae8c1d8682
Moe Thet Khine <soniapoudel03@gmail.com>
2026-04-04 10:05:40 +0630

    Feat : Add GetBlogsAsync in BlogRepository

--- OnionArchitecture.Infrastructure/Features/Blog/BlogRepository.cs
+++ OnionArchitecture.Infrastructure/Features/Blog/BlogRepository.cs
@@ -1,4 +1,9 @@
-﻿using OnionArchitecture.DbService.AppDbContextModels;
+﻿using Microsoft.EntityFrameworkCore;
+using OnionArchitecture.DbService.AppDbContextModels;
+using OnionArchitecture.DTOs.Features.Blog;
+using OnionArchitecture.DTOs.Features.PageSetting;
+using OnionArchitecture.Shared;
+using OnionArchitecture.Utils;
 
 namespace OnionArchitecture.Infrastructure.Features.Blog;
 
@@ -9,5 +14,46 @@ public class BlogRepository : IBlogRepository
 	public BlogRepository(AppDbContext appDbContext)
 	{
 		_appDbContext = appDbContext;
+	}
+
+	public async Task<Result<BlogListModelV1>> GetBlogsAsync(int pageNo, int pageSize, CancellationToken cancellationToken)
+	{
+		Result<BlogListModelV1> result;
+
+		try
+		{
+			var query = _appDbContext.TblBlogs.OrderByDescending(x => x.BlogId);
+			var lst = await query
+				.Paginate(pageNo, pageSize)
+				.ToListAsync(cancellationToken: cancellationToken);
+			var totalCount = await query.CountAsync(cancellationToken: cancellationToken);
+			var pageCount = totalCount / pageSize;
+			if (totalCount % pageSize > 0)
+			{
+				pageCount++;
+			}
+
+			var pageSettingModel = new PageSettingModel(pageNo, pageSize, pageCount, totalCount);
+			var model = new BlogListModelV1()
+			{
+				DataLst = lst.Select(x => new BlogModel()
+				{
+					BlogId = x.BlogId,
+					BlogTitle = x.BlogTitle,
+					BlogAuthor = x.BlogAuthor,
+					BlogContent = x.BlogContent
+				})
+					.AsQueryable(),
+				PageSetting = pageSettingModel
+			};
+
+			result = Result<BlogListModelV1>.Success(model);
+		}
+
+		catch (Exception ex)
+		{
+			result = Result<BlogListModelV1>.Failure(ex);
+		}
+		return result;
 	}
 }
