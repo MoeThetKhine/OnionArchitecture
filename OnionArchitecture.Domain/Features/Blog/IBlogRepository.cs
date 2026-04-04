f70a62fb5d4a2bfc314822a33259a1a947475b26
Moe Thet Khine <soniapoudel03@gmail.com>
2026-04-04 09:59:22 +0630

    Feat : Add UpdateBlogAsync in IBlogRepository

--- OnionArchitecture.Domain/Features/Blog/IBlogRepository.cs
+++ OnionArchitecture.Domain/Features/Blog/IBlogRepository.cs
@@ -8,5 +8,6 @@ public interface IBlogRepository
 	Task<Result<BlogListModelV1>> GetBlogsAsync(int pageNo, int pageSize, CancellationToken cancellationToken);
 	Task<Result<BlogModel>> GetBlogByIdAsync(int id, CancellationToken cancellationToken);
 	Task<Result<BlogModel>> CreateBlogAsync(BlogRequestModel blogRequest, CancellationToken cancellationToken);
+	Task<Result<BlogModel>> UpdateBlogAsync(BlogRequestModel blogRequest, int id, CancellationToken cancellationToken);
 
 }
