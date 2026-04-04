a20aad8e38127091e30e8e9c1ba681097ccd5369
Moe Thet Khine <soniapoudel03@gmail.com>
2026-04-04 10:57:10 +0630

    Feat : Add GetBlogsAsync in BlogController

--- OnionArchitecture.Presentation/Controllers/Blog/BlogController.cs
+++ OnionArchitecture.Presentation/Controllers/Blog/BlogController.cs
@@ -1,10 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using OnionArchitecture.Application.Features.Blog.GetBlog;
 
 namespace OnionArchitecture.Presentation.Controllers.Blog;
 
 [Route("api/[controller]")]
 [ApiController]
-public class BlogController : ControllerBase
+public class BlogController : BaseController
 {
-	
+	private readonly IMediator _mediator;
+
+	public BlogController(IMediator mediator)
+	{
+		_mediator = mediator;
+	}
+
+	[HttpGet]
+	public async Task<IActionResult> GetBlogsAsync(int pageNo, int pageSize, CancellationToken cancellationToken)
+	{
+		var query = new GetBlogListQuery(pageNo, pageSize);
+		var result = await _mediator.Send(query, cancellationToken);
+
+		return Content(result);
+	}
 }
