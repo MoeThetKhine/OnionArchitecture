4647603649c491633e8084117bb6b54c2c234bfa
Moe Thet Khine <soniapoudel03@gmail.com>
2026-04-05 21:01:00 +0630

    Format Code

--- OnionArchitecture.Application/Features/Blog/PatchBlog/PatchBlogCommand.cs
+++ OnionArchitecture.Application/Features/Blog/PatchBlog/PatchBlogCommand.cs
@@ -1,16 +1,15 @@
-﻿namespace OnionArchitecture.Application.Features.Blog.PatchBlog
+﻿namespace OnionArchitecture.Application.Features.Blog.PatchBlog;
+
+public class PatchBlogCommand : IRequest<Result<BlogModel>>
 {
-	public class PatchBlogCommand : IRequest<Result<BlogModel>>
+	public BlogRequestModel BlogRequestModel { get; set; }
+
+	public int BlogId { get; set; }
+
+	public PatchBlogCommand(BlogRequestModel blogRequestModel, int blogId)
 	{
-		public BlogRequestModel BlogRequestModel { get; set; }
-
-		public int BlogId { get; set; }
-
-		public PatchBlogCommand(BlogRequestModel blogRequestModel, int blogId)
-		{
-			BlogRequestModel = blogRequestModel;
-			BlogId = blogId;
-		}
-
+		BlogRequestModel = blogRequestModel;
+		BlogId = blogId;
 	}
+
 }
