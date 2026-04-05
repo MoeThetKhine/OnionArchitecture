7f22f45d5fb1944bc28f82f1a2393c40989f0146
Moe Thet Khine <soniapoudel03@gmail.com>
2026-04-05 20:56:09 +0630

    Feat : Add PatchBlogCommand

--- OnionArchitecture.Application/Features/Blog/PatchBlog/PatchBlogCommand.cs
+++ OnionArchitecture.Application/Features/Blog/PatchBlog/PatchBlogCommand.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace OnionArchitecture.Application.Features.Blog.PatchBlog
 {
-	internal class PatchBlogCommand
+	public class PatchBlogCommand : IRequest<Result<BlogModel>>
 	{
+		public BlogRequestModel BlogRequestModel { get; set; }
+
+		public int BlogId { get; set; }
+
+		public PatchBlogCommand(BlogRequestModel blogRequestModel, int blogId)
+		{
+			BlogRequestModel = blogRequestModel;
+			BlogId = blogId;
+		}
+
 	}
 }
