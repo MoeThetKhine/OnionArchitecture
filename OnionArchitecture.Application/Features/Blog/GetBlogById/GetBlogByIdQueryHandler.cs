4461271d43535f5bfae84f77ee81c586d288e154
Moe Thet Khine <soniapoudel03@gmail.com>
2026-04-05 12:46:32 +0630

    Feat : Add GetBlogByIdQueryHandler

--- OnionArchitecture.Application/Features/Blog/GetBlogById/GetBlogByIdQueryHandler.cs
+++ OnionArchitecture.Application/Features/Blog/GetBlogById/GetBlogByIdQueryHandler.cs
@@ -1,5 +1,28 @@
 ﻿namespace OnionArchitecture.Application.Features.Blog.GetBlogById;
 
-public class GetBlogByIdQueryHandler
+public class GetBlogByIdQueryHandler : IRequestHandler<GetBlogByIdQuery, Result<BlogModel>>
 {
+	private readonly IBlogRepository _blogRepository;
+
+	public GetBlogByIdQueryHandler(IBlogRepository blogRepository)
+	{
+		_blogRepository = blogRepository;
+	}
+
+	public async Task<Result<BlogModel>> Handle(GetBlogByIdQuery request, CancellationToken cancellationToken)
+	{
+		Result<BlogModel> result;
+
+		if (request.BlogId <= 0)
+		{
+			result = Result<BlogModel>.Fail(MessageResource.InvalidId);
+			goto result;
+		}
+
+		result = await _blogRepository.GetBlogByIdAsync(request.BlogId, cancellationToken);
+
+	result:
+		return result;
+	}
+
 }
