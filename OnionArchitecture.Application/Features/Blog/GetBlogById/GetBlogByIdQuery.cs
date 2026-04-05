c202885559967c53bf8adbb17e71d97ab3ea49ef
Moe Thet Khine <soniapoudel03@gmail.com>
2026-04-05 12:44:54 +0630

    Feat : Add GetBlogByIdQuery

--- OnionArchitecture.Application/Features/Blog/GetBlogById/GetBlogByIdQuery.cs
+++ OnionArchitecture.Application/Features/Blog/GetBlogById/GetBlogByIdQuery.cs
@@ -1,5 +1,11 @@
 ﻿namespace OnionArchitecture.Application.Features.Blog.GetBlogById;
 
-public class GetBlogByIdQuery
+public class GetBlogByIdQuery : IRequest<Result<BlogModel>>
 {
+	public int BlogId { get; set; }
+
+	public GetBlogByIdQuery(int blogId)
+	{
+		BlogId = blogId;
+	}
 }
