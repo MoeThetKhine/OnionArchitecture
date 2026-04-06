c45be65680e5ceafc58760be88cc6974ec555316
Moe Thet Khine <soniapoudel03@gmail.com>
2026-04-06 17:48:54 +0630

    Feat : Add PatchBlogCommandHandler

--- OnionArchitecture.Application/Features/Blog/PatchBlog/PatchBlogCommandHandler.cs
+++ OnionArchitecture.Application/Features/Blog/PatchBlog/PatchBlogCommandHandler.cs
@@ -1,6 +1,31 @@
-﻿namespace OnionArchitecture.Application.Features.Blog.PatchBlog
+﻿using OnionArchitecture.Domain.Features.Blog;
+
+namespace OnionArchitecture.Application.Features.Blog.PatchBlog
 {
-	public class PatchBlogCommandHandler
+	public class PatchBlogCommandHandler : IRequestHandler<PatchBlogCommand, Result<BlogModel>>
 	{
+		private readonly IBlogRepository repository;
+
+		public PatchBlogCommandHandler(IBlogRepository repository)
+		{
+			this.repository = repository;
+		}
+
+		public async Task<Result<BlogModel>> Handle(PatchBlogCommand request, CancellationToken cancellationToken)
+		{
+			Result<BlogModel> result;
+
+			if (request.BlogId <= 0)
+			{
+				result = Result<BlogModel>.Fail(MessageResource.InvalidId);
+				goto result;
+			}
+
+			result = await _blogRepository.PatchBlogAsync(request.BlogRequestModel, request.BlogId, cancellationToken);
+
+		result:
+			return result;
+		}
+
 	}
 }
