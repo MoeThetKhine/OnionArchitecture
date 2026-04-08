1755c1a298ebb96f0891209f9c18fdf0403a24ed
Moe Thet Khine <soniapoudel03@gmail.com>
2026-04-08 09:12:39 +0630

    Format Code

--- OnionArchitecture.Application/Features/Blog/PatchBlog/PatchBlogCommandHandler.cs
+++ OnionArchitecture.Application/Features/Blog/PatchBlog/PatchBlogCommandHandler.cs
@@ -1,29 +1,28 @@
-﻿namespace OnionArchitecture.Application.Features.Blog.PatchBlog
+﻿namespace OnionArchitecture.Application.Features.Blog.PatchBlog;
+
+public class PatchBlogCommandHandler : IRequestHandler<PatchBlogCommand, Result<BlogModel>>
 {
-	public class PatchBlogCommandHandler : IRequestHandler<PatchBlogCommand, Result<BlogModel>>
+	private readonly IBlogRepository repository;
+
+	public PatchBlogCommandHandler(IBlogRepository repository)
 	{
-		private readonly IBlogRepository repository;
-
-		public PatchBlogCommandHandler(IBlogRepository repository)
-		{
-			this.repository = repository;
-		}
-
-		public async Task<Result<BlogModel>> Handle(PatchBlogCommand request, CancellationToken cancellationToken)
-		{
-			Result<BlogModel> result;
-
-			if (request.BlogId <= 0)
-			{
-				result = Result<BlogModel>.Fail(MessageResource.InvalidId);
-				goto result;
-			}
-
-			result = await _blogRepository.PatchBlogAsync(request.BlogRequestModel, request.BlogId, cancellationToken);
-
-		result:
-			return result;
-		}
-
+		this.repository = repository;
 	}
+
+	public async Task<Result<BlogModel>> Handle(PatchBlogCommand request, CancellationToken cancellationToken)
+	{
+		Result<BlogModel> result;
+
+		if (request.BlogId <= 0)
+		{
+			result = Result<BlogModel>.Fail(MessageResource.InvalidId);
+			goto result;
+		}
+
+		result = await _blogRepository.PatchBlogAsync(request.BlogRequestModel, request.BlogId, cancellationToken);
+
+	result:
+		return result;
+	}
+
 }
